fefcf7ab3db199c9628652fd1176126ef8607c99
huanxiaomang <atri2022@163.com>
2024-04-20 15:37:55 +0800

    fix: 修复start脚本

--- admin/start.sh
+++ admin/start.sh
@@ -4,10 +4,14 @@ echo 正在启动项目
 
 cd dist
 
+git init
+
+git remote add origin https://github.com/huanxiaomang/cz-official.git
+
 echo 正在拉取编译后的代码...
 
 
-git pull -f https://github.com/huanxiaomang/cz-official.git master:admin-pages
+git pull -f origin admin-pages
 
 npx http-server . -p 7000
 
