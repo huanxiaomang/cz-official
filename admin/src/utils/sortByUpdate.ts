ce42264044161e95ccc1ebcf3677faf0c7719797
huanxiaomang <atri2022@163.com>
2024-04-23 11:57:23 +0800

    repo: 改为monorepo，把admin逐出本仓库

--- admin/src/utils/sortByUpdate.ts
+++
@@ -1,8 +0,0 @@
-export const toTimestamp = (time: string) => {
-    const date = new Date(time);
-    return date.getTime();
-}
-
-export const sortByUpdate = (list: { updatedAt: string }[]) => {
-    return list.toSorted((a, b) => toTimestamp(b.updatedAt) - toTimestamp(a.updatedAt));
-}
