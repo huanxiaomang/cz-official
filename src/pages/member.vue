028e362089efa8b05e56d16cf8d684ae86790649
huanxiaomang <atri2022@163.com>
2024-05-12 18:01:50 +0800

    fix: 修复member页移动端布局错误

--- src/pages/member.vue
+++ src/pages/member.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div container m-auto id="memberlist">
-      <div v-masonry fit-width="true" transition-duration="0s" origin-left="false" w-full m-auto
+      <div v-masonry fit-width="true" transition-duration="0s" origin-left="false" w-full m-auto gap-10
         container>
         <MemberCard v-masonry-tile v-for="user of userList" :userInfo="user"
-          class="card"></MemberCard>
+          class="card" w-80 mx-auto md:mx-6></MemberCard>
       </div>
     </div>
   </div>
