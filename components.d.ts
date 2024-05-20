c1d22aa9954c27a9297a3d24925fd6b332d723f1
huanxiaomang <atri2022@163.com>
2024-05-20 20:26:23 +0800

    refactor: 使用map优化了useStackIcon

--- components.d.ts
+++ components.d.ts
@@ -7,17 +7,6 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    AButton: typeof import('ant-design-vue/es')['Button']
-<<<<<<< HEAD
-=======
-    ACheckbox: typeof import('ant-design-vue/es')['Checkbox']
->>>>>>> 8e2025677c7c8cc8ccc401f311abd9924e3b0dc9
-    AForm: typeof import('ant-design-vue/es')['Form']
-    AFormItem: typeof import('ant-design-vue/es')['FormItem']
-    AInput: typeof import('ant-design-vue/es')['Input']
-    ASelect: typeof import('ant-design-vue/es')['Select']
-    ASelectOption: typeof import('ant-design-vue/es')['SelectOption']
-    ATextarea: typeof import('ant-design-vue/es')['Textarea']
     Badge: typeof import('./src/components/Badge.vue')['default']
     CZAvatar: typeof import('./src/components/CZAvatar.vue')['default']
     Footer: typeof import('./src/components/layout/footer.vue')['default']
