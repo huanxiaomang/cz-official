003b5b12e80e1984061f7415a68d12dd979af98f
huanxiaomang <atri2022@163.com>
2024-05-24 21:08:04 +0800

    fix: github action指令错误

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,14 +5,14 @@
 </template>
 
 <script setup lang="ts" generic="T extends any, O extends any">
-import axios from 'axios';
-import { onMounted, ref } from 'vue';
 import IndexVideo from "../components/view/IndexVideo.vue"
 import { useRouter } from 'vue-router';
 const router= useRouter();
 defineOptions({
   name: 'IndexPage',
 })
+
+
 
 
 </script>
