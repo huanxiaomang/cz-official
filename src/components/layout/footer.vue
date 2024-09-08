9ca7c8746af76d367076a5f31404c94a7a94a923
huanxiaomang <atri2022@163.com>
2024-09-08 18:11:03 +0800

    chore: 添加备案信息

--- src/components/layout/footer.vue
+++ src/components/layout/footer.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav items-center w-full border-b-bluegray h-55 sm:h-50 text-gray-300 flex flex-col sm:flex-row
-    justify-center gap-1 sm:gap-8 select-none absolute bottom-0 left-0 class="dark:bg-gradient-to-dark">
+  <nav items-center w-full border-b-bluegray h-55 sm:h-50 text-gray-300 flex flex-col sm:flex-row justify-center gap-1
+    sm:gap-8 select-none absolute bottom-0 left-0 class="dark:bg-gradient-to-dark">
     <img src="./../../assets/icon/cz_ba-style_white.png" alt="logo" h-20 sm:h-25>
     <div text-center sm:text-left>
       <div class="cn" pb-2>
@@ -10,6 +10,7 @@
       </div>
       <div>Copyright © 2023-present 创智工作室</div>
       <div>Released under the MIT License.</div>
+      <div>吉ICP备2024018770号</div>
       <img src="./../../assets/icon/beihua-remove.png" alt="北华大学" h-4 sm:h-8 mt-2 invisible sm:visible>
     </div>
 
